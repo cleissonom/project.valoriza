b52de912d67af82b77c5b4cce2bb29db9c981c84
Cleisson <cleissonconstc@gmail.com>
2021-10-06 10:14:38 -0300

    adding tsconfig

---
+++ index.js
@@ -0,0 +1,4 @@
+function sendEmail(name, email) {
+    console.log("Send email to " + name + " com email " + email);
+}
+sendEmail("Cleisson", "cleissonconstc@gmail.com");
---
+++ index.ts
@@ -0,0 +1,5 @@
+function sendEmail(name: string, email: string) {
+   console.log(`Send email to ${name} com email ${email}`);
+}
+
+sendEmail("Cleisson", "cleissonconstc@gmail.com")
