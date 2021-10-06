9dc2d3801116b9c168ff96a77d3006c1e068fecf
Cleisson <cleissonconstc@gmail.com>
2021-10-06 10:43:31 -0300

    creatin method get in server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
 import express from "express";
 
+// @types/express
 const app = express();
+
+/*
+   GET    => Buscar uma informação
+   POST   => Inserir (Criar) uma informação
+   PUT    => Alterar uma informação
+   DELETE => Remover um dado
+   PATCH  => Alterar uma informação específica
+*/
+
+app.get('/test', (req, res) => {
+   // Reqeust => Entrando
+   // Response => Saindo
+   res.send('Olá Cleisson')
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log('Server running...'))
